710cbb1fed2ed79b877c4f105ee4ae52e97fb6e7
Jason Baker <jason.th.baker@gmail.com>
2019-01-11 23:47:28 -0500

    Updates Please enter the commit message for your changes. Lines starting

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,8 +3,8 @@ osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: "OpenStreetmap"
 })
 
-aerial = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',{
-    attribution: "Google Aerial",id:'aerial'
+aerial = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+    attribution: "Google Aerial", id: 'aerial'
 })
 m = L.map('mymap', {
     center: [40, -92],
@@ -13,7 +13,7 @@ m = L.map('mymap', {
 })
 
 let basemaps = {
-    "Aerial":aerial,
+    "Aerial": aerial,
     "OpenStreetMap": osm
 }
 let drawnItems = L.geoJson().addTo(m)
@@ -24,7 +24,8 @@ let overlays = {
 }
 
 L.control.layers(basemaps, overlays, {
-    collapsed: false}
+    collapsed: false
+}
 ).addTo(m)
 
 
@@ -63,8 +64,8 @@ var template = function (properties) {
     return `<form id="popup-form">\
     <table>\
     <tbody>\
-    ${Object.keys(props).map(k=>{
-        return `<tr><th><label for='${k}'>${k.toUpperCase()}</th><td><input type='${props[k].type}' id='${k}' value='${properties[k]?properties[k]:""}'></td></tr>`
+    ${Object.keys(props).map(k => {
+        return `<tr><th><label for='${k}'>${k.toUpperCase()}</th><td><input type='${props[k].type}' id='${k}' value='${properties[k] ? properties[k] : ""}'></td></tr>`
     }).join('')}</tbody>\
     </table>\
     </form>`
@@ -73,9 +74,9 @@ var template = function (properties) {
 m.on('draw:created', function (event) {
     let layer = event.layer;
     let feature = layer.feature = layer.feature || {}
-    feature.type='Feature'
+    feature.type = 'Feature'
     console.log(layer)
-    feature.properties={}
+    feature.properties = {}
     Object.keys(props).forEach(v => {
         feature.properties[v] = null
     })
@@ -88,49 +89,45 @@ drawnItems.bindPopup(popup, {
 
 let popform = document.getElementById('popup-form')
 
-// drawnItems.bindTooltip(function (layer,feature) {
-//     return JSON.stringify(layer.feature.properties)
-// })
-
-m.on('popupopen', function(layer,feature) {
+m.on('popupopen', function (layer, feature) {
     console.log(layer, feature)
     let popup = document.querySelector('#editorsubmit')
-    popup.addEventListener('click',function(e){
+    popup.addEventListener('click', function (e) {
         console.log(e.target.toGeoJSON())
     });
-  });
+});
 
 
-function popup(layer, feature){
+function popup(layer, feature) {
     let div = L.DomUtil.create('div', 'edtiormenu')
     // let inp = template(feature.properties)
     let table = L.DomUtil.create('table')
     let tbody = L.DomUtil.create('tbody')
     table.appendChild(tbody)
     div.appendChild(table)
-    Object.keys(props).forEach(e=>{
+    Object.keys(props).forEach(e => {
         let tr = L.DomUtil.create('tr')
         let th = L.DomUtil.create('th')
         let td = L.DomUtil.create('td')
-        let label=L.DomUtil.create('label')
-        let inp=L.DomUtil.create('input')
+        let label = L.DomUtil.create('label')
+        let inp = L.DomUtil.create('input')
         th.appendChild(label)
         td.appendChild(inp)
         tr.appendChild(th)
         tr.appendChild(td)
-        label.innerText=e
-        inp.type=props[e].type
-        inp.id=e
-        inp.value=layer.feature.properties[e]
-        inp.addEventListener('change',function(t){
-            layer.feature.properties[e]=t.target.value
+        label.innerText = e
+        inp.type = props[e].type
+        inp.id = e
+        inp.value = layer.feature.properties[e]
+        inp.addEventListener('change', function (t) {
+            layer.feature.properties[e] = t.target.value
             console.log(layer.toGeoJSON())
         })
         tbody.appendChild(tr)
     })
     button = document.createElement('button')
-    button.innerText='Hello'
-    button.id="editorsubmit"
+    button.innerText = 'Hello'
+    button.id = "editorsubmit"
     div.append(button)
     return div
 }
@@ -138,7 +135,7 @@ function popup(layer, feature){
 let geocoder = L.Control.geocoder({
     defaultMarkGeocode: false
 })
-.on('markgeocode', function(e) {
-    let center = e.geocode.center
-    m.flyTo(L.latLng(center.lat, center.lng),15);
-}).addTo(m);
+    .on('markgeocode', function (e) {
+        let center = e.geocode.center
+        m.flyTo(L.latLng(center.lat, center.lng), 15);
+    }).addTo(m);
