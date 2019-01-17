12bff02574666759c3249585c20fbd560e101aad
Jason Baker <jason.th.baker@gmail.com>
2019-01-17 13:23:05 -0500

    Remove unnecessary script import

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# geofencing
-GeofencingFreightwaves
+# GeoFencing
+A REST Web application built using the [Python-Responder](https://python-responder.org/en/latest/) web framework for the back end, and Vue as well as Leaflet on the front end.
+
+
--- db.py
+++ db.py
@@ -39,22 +39,21 @@ class Business(Base, BaseTable):
     id = sql.Column("id", sqlalchemy_utils.UUIDType(binary=True), primary_key=True)
     address = sql.Column("formatted_address", sql.VARCHAR)
     name = sql.Column("name", sql.VARCHAR)
-    assigned = sql.Column('assigned', sql.Boolean)
+    assigned = sql.Column("assigned", sql.Boolean)
     edited = sql.Column("Edited", sql.Boolean)
     priority = sql.Column("priority", sql.Integer)
     editor = sql.Column("editor", sql.VARCHAR)
 
     def handle_type(self, attr):
-        if hasattr(getattr(self, attr), 'hex'):
-            return getattr(getattr(self, attr), 'hex')
+        if hasattr(getattr(self, attr), "hex"):
+            return getattr(getattr(self, attr), "hex")
         else:
             return getattr(self, attr)
-        
 
     def to_json(self):
         return {
             name: self.handle_type(name)
-            for name in ["id", "address", "name", "edited", "priority","assigned"]
+            for name in ["id", "address", "name", "edited", "priority", "assigned"]
         }
 
 
